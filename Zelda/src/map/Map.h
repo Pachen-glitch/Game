5049596b91169782459676b493f8ef451441fcbd
Jorge <jjmarcant06@gmail.com>
2026-05-25 12:10:53 -0600

    FIX MAP

--- Zelda/src/map/Map.h
+++ Zelda/src/map/Map.h
@@ -1,43 +1,29 @@
 #pragma once
 
-
 #include <vector>
 
-
-
-enum TileType {
-
+enum class TileType {
     FLOOR,
-    WALL
+    WALL,
+    DOOR,
+    LOCKED_DOOR
 };
 
-
-
-
-
 class Map {
+public:
+    Map();
+    explicit Map(int width, int height);
+
+    TileType getTile(int x, int y) const;
+    void setTile(int x, int y, TileType tile);
+
+    int getWidth() const;
+    int getHeight() const;
+
+    void fill(TileType tile);
 
 private:
-
     std::vector<std::vector<TileType>> grid;
 
-public:
-
-    
-    Map();
-
-   
-
-    TileType getTile(int x, int y);
-
-    void setTile(
-        int x,
-        int y,
-        TileType tile
-    );
-
-   
-    int getWidth();
-
-    int getHeight();
+    void initGrid(int w, int h, TileType fillType);
 };
